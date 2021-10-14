46de6f22ee59248742365ae3784c5d0e9c25a3d1
nellfs <pokycelit@gmail.com>
2021-10-14 18:45:12 -0300

    a triangle :)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,81 @@
-#[allow(unused_imports)]
-#[macro_use]
-extern crate glium;
-use glium::{glutin, Surface};
+use glium::implement_vertex;
 
-fn main() {
+//use glium::glutin::event_loop;
+
+extern crate glium;
+
+fn main(){
+    use glium::glutin;
+    use glium::Surface;
+
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new()
-    .with_title("Game-Engine");
+    .with_title("Game Engine");
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let mut display = display.draw();
-    display.clear_color(0.0, 0.0, 0.0, 1.0);
-    display.finish().unwrap();
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    implement_vertex!(Vertex, position);
 
-    // loop
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = match event {
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.5] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+    #version 140
+
+    in vec2 position;
+
+    void main(){
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    event_loop.run(move |ev, _, control_flow|{
+
+        //FRAME LOOP
+        //let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        //*control_flow == glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match ev {
             glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => glutin::event_loop::ControlFlow::Exit,
-                glutin::event::WindowEvent::Resized(..) => {
-                    //draw();
-                    glutin::event_loop::ControlFlow::Poll
+                glutin::event::WindowEvent::CloseRequested => {
+                *control_flow = glutin::event_loop::ControlFlow::Exit;
+                return;
                 },
-                _ => glutin::event_loop::ControlFlow::Poll,
+                _ => return,
             },
-            _ => glutin::event_loop::ControlFlow::Poll,
-        };
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => (),
+        }
+
+        // COLORS
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+
     });
 }
