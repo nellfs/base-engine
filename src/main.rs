dcefc290d2c74950be9a73b15113dcb0b1533d08
nellfs <pokycelit@gmail.com>
2021-12-20 08:52:41 -0300

    OpenGL window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+extern crate gl;
+extern crate glfw;
+use std::sync::mpsc::Receiver;
+
+use self::glfw::{Action, Context, Key};
+
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
 fn main() {
-    print!("Hello World")
+    //Configure
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    //Window
+    let (mut window, events) = glfw
+        .create_window(
+            SCR_WIDTH,
+            SCR_HEIGHT,
+            "OpenGL window",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window");
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    //gl: Load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    // Render loop
+    while !window.should_close() {
+        //Events
+        process_events(&mut window, &events);
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                gl::Viewport(0, 0, width, height)
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
+        }
+    }
 }
