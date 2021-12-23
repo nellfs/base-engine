6b3728c64e148426e188a48eab9f1035e4eba32f
nellfs <pokycelit@gmail.com>
2021-12-23 14:06:21 -0300

    texture test

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ use self::glfw::{Action, Context, Key};
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
 
-#[allow(non_snake_case)]
 fn main() {
     //Configure
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -43,28 +42,28 @@ fn main() {
     //gl: Load all OpenGL function pointers
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    let (ourShader, VAO) = unsafe {
-        let ourShader = Shader::new("src/shaders/shader.vs", "src/shaders/shader.fs");
+    let (our_shader, vao, texture) = unsafe {
+        let our_shader = Shader::new("src/shaders/shader.vs", "src/shaders/shader.fs");
 
-        let vertices: [f32; 18] = [
-            // positions    // colors
-            0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
-            -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
-            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+        let vertices: [f32; 32] = [
+            // positions       // colors        // texture coords
+            0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
+            -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom left
+            -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
         ];
-
-        let (mut VBO, mut VAO) = (0, 0);
-        gl::GenVertexArrays(1, &mut VAO);
-        gl::GenBuffers(1, &mut VBO);
-        gl::BindVertexArray(VAO);
-        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        let (mut vbo, mut vao) = (0, 0);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER,
             mem::size_of_val(&vertices) as GLsizeiptr,
             vertices.as_ptr() as *const c_void,
             gl::STATIC_DRAW,
         );
-        let stride = 6 * mem::size_of::<GLfloat>() as GLsizei;
+        let stride = 8 * mem::size_of::<GLfloat>() as GLsizei;
 
         //Position
         gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
@@ -81,7 +80,48 @@ fn main() {
         );
         gl::EnableVertexAttribArray(1);
 
-        (ourShader, VAO)
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            stride,
+            (6 * mem::size_of::<GLfloat>()) as *const c_void,
+        );
+        gl::EnableVertexAttribArray(2);
+
+        //Load and create a texture
+        let mut texture = 0;
+        gl::GenTextures(1, &mut texture);
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+
+        //Set the texture wrapping parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+
+        //Set texture filterting parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+        let img = image::open("assets/wall.jpg").unwrap().to_rgb8();
+        let img_width = img.width() as i32;
+        let img_height = img.height() as i32;
+        let data: Vec<u8> = img.into_raw();
+
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            gl::RGB as i32,
+            img_width,
+            img_height,
+            0,
+            gl::RGB,
+            gl::UNSIGNED_BYTE,
+            &data[0] as *const u8 as *const c_void,
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+
+        (our_shader, vao, texture)
     };
 
     // Render loop
@@ -94,9 +134,12 @@ fn main() {
             gl::ClearColor(0.1, 0.1, 0.1, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            ourShader.UseProgram();
-            gl::BindVertexArray(VAO);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            //Bind texture
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+
+            our_shader.useprogram();
+            gl::BindVertexArray(vao);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
         }
 
         // GLFW: Swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
--- src/shader.rs
+++ src/shader.rs
@@ -1,97 +1,72 @@
-#[allow(non_snake_case)]
-use std::{
-    ffi::{CStr, CString},
-    fs::File,
-    io::Read,
-    ptr,
-};
+use std::{ffi::CString, fs::File, io::Read, ptr};
 
 use gl::types::{GLchar, GLint};
 
 pub struct Shader {
-    pub ID: u32,
+    pub id: u32,
 }
 
 impl Shader {
-    pub fn new(vertexPath: &str, fragmentPath: &str) -> Shader {
-        let mut shader = Shader { ID: 0 };
+    pub fn new(vertex_path: &str, fragment_path: &str) -> Shader {
+        let mut shader = Shader { id: 0 };
 
         // Retrive the vertex/fragment source code from filesystem
-        let mut vShaderFile =
-            File::open(vertexPath).unwrap_or_else(|_| panic!("Failed to open {}", vertexPath));
-        let mut fShaderFile =
-            File::open(fragmentPath).unwrap_or_else(|_| panic!("Failed to open {}", fragmentPath));
+        let mut v_shaderfile =
+            File::open(vertex_path).unwrap_or_else(|_| panic!("Failed to open {}", vertex_path));
+        let mut f_shaderfile = File::open(fragment_path)
+            .unwrap_or_else(|_| panic!("Failed to open {}", fragment_path));
 
-        let mut vertexCode = String::new();
-        let mut fragmentCode = String::new();
+        let mut vertex_code = String::new();
+        let mut fragment_code = String::new();
 
-        vShaderFile
-            .read_to_string(&mut vertexCode)
+        v_shaderfile
+            .read_to_string(&mut vertex_code)
             .expect("Failed to read vertex shader");
-        fShaderFile
-            .read_to_string(&mut fragmentCode)
+        f_shaderfile
+            .read_to_string(&mut fragment_code)
             .expect("Failed to read fragment shader");
 
-        let vShaderCode = CString::new(vertexCode.as_bytes()).unwrap();
-        let fShaderCode = CString::new(fragmentCode.as_bytes()).unwrap();
+        let v_shadercode = CString::new(vertex_code.as_bytes()).unwrap();
+        let f_shadercode = CString::new(fragment_code.as_bytes()).unwrap();
 
         //Compile shaders
         unsafe {
             //Vertex
             let vertex = gl::CreateShader(gl::VERTEX_SHADER);
-            gl::ShaderSource(vertex, 1, &vShaderCode.as_ptr(), ptr::null());
+            gl::ShaderSource(vertex, 1, &v_shadercode.as_ptr(), ptr::null());
             gl::CompileShader(vertex);
-            shader.checkCompileErrors(vertex, "VERTEX");
+            shader.checkcompileerros(vertex, "VERTEX");
 
             //Fragment
             let fragment = gl::CreateShader(gl::FRAGMENT_SHADER);
-            gl::ShaderSource(fragment, 1, &fShaderCode.as_ptr(), ptr::null());
+            gl::ShaderSource(fragment, 1, &f_shadercode.as_ptr(), ptr::null());
             gl::CompileShader(fragment);
-            shader.checkCompileErrors(fragment, "FRAGMENT");
+            shader.checkcompileerros(fragment, "FRAGMENT");
 
             //Program
-
-            let ID = gl::CreateProgram();
-            gl::AttachShader(ID, vertex);
-            gl::AttachShader(ID, fragment);
-            gl::LinkProgram(ID);
-            shader.checkCompileErrors(ID, "PROGRAM");
+            let id = gl::CreateProgram();
+            gl::AttachShader(id, vertex);
+            gl::AttachShader(id, fragment);
+            gl::LinkProgram(id);
+            shader.checkcompileerros(id, "PROGRAM");
 
             gl::DeleteShader(vertex);
             gl::DeleteShader(fragment);
-            shader.ID = ID;
+            shader.id = id;
         }
         shader
     }
 
     //Activate the shader
-    pub unsafe fn UseProgram(&self) {
-        gl::UseProgram(self.ID)
+    pub unsafe fn useprogram(&self) {
+        gl::UseProgram(self.id)
     }
 
-    //Utitity uniform functions
-    pub unsafe fn setBoll(&self, name: &CStr, value: bool) {
-        gl::Uniform1i(gl::GetUniformLocation(self.ID, name.as_ptr()), value as i32);
-    }
-
-    pub unsafe fn setInt(&self, name: &CStr, value: i32) {
-        gl::Uniform1i(gl::GetUniformLocation(self.ID, name.as_ptr()), value);
-    }
-
-    pub unsafe fn setFloat(&self, name: &CStr, value: f32) {
-        gl::Uniform1f(gl::GetUniformLocation(self.ID, name.as_ptr()), value);
-    }
-
-    pub unsafe fn setVec3(&self, name: &CStr, x: f32, y: f32, z: f32) {
-        gl::Uniform3f(gl::GetUniformLocation(self.ID, name.as_ptr()), x, y, z);
-    }
-
-    /// utility function for checking shader compilation/linking errors.
-    /// ------------------------------------------------------------------------
-    unsafe fn checkCompileErrors(&self, shader: u32, type_: &str) {
+    ///Utility function for checking shader compilation/linking errors.
+    unsafe fn checkcompileerros(&self, shader: u32, type_: &str) {
         let mut success = gl::FALSE as GLint;
-        let mut infoLog = Vec::with_capacity(1024);
-        infoLog.set_len(1024 - 1); // subtract 1 to skip the trailing null character
+        let mut info_log = Vec::with_capacity(1024);
+        info_log.set_len(1024 - 1); // subtract 1 to skip the trailing null character
         if type_ != "PROGRAM" {
             gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
             if success != gl::TRUE as GLint {
@@ -99,13 +74,13 @@ impl Shader {
                     shader,
                     1024,
                     ptr::null_mut(),
-                    infoLog.as_mut_ptr() as *mut GLchar,
+                    info_log.as_mut_ptr() as *mut GLchar,
                 );
                 println!(
                     "ERROR::SHADER_COMPILATION_ERROR of type: {}\n{}\n \
                           -- --------------------------------------------------- -- ",
                     type_,
-                    std::str::from_utf8(&infoLog).unwrap()
+                    std::str::from_utf8(&info_log).unwrap()
                 );
             }
         } else {
@@ -115,77 +90,15 @@ impl Shader {
                     shader,
                     1024,
                     ptr::null_mut(),
-                    infoLog.as_mut_ptr() as *mut GLchar,
+                    info_log.as_mut_ptr() as *mut GLchar,
                 );
                 println!(
                     "ERROR::PROGRAM_LINKING_ERROR of type: {}\n{}\n \
                           -- --------------------------------------------------- -- ",
                     type_,
-                    std::str::from_utf8(&infoLog).unwrap()
+                    std::str::from_utf8(&info_log).unwrap()
                 );
             }
         }
-    }
-
-    /// Only used in 4.9 Geometry shaders - ignore until then (shader.h in original C++)
-    pub fn with_geometry_shader(vertexPath: &str, fragmentPath: &str, geometryPath: &str) -> Self {
-        let mut shader = Shader { ID: 0 };
-        // 1. retrieve the vertex/fragment source code from filesystem
-        let mut vShaderFile =
-            File::open(vertexPath).unwrap_or_else(|_| panic!("Failed to open {}", vertexPath));
-        let mut fShaderFile =
-            File::open(fragmentPath).unwrap_or_else(|_| panic!("Failed to open {}", fragmentPath));
-        let mut gShaderFile =
-            File::open(geometryPath).unwrap_or_else(|_| panic!("Failed to open {}", geometryPath));
-        let mut vertexCode = String::new();
-        let mut fragmentCode = String::new();
-        let mut geometryCode = String::new();
-        vShaderFile
-            .read_to_string(&mut vertexCode)
-            .expect("Failed to read vertex shader");
-        fShaderFile
-            .read_to_string(&mut fragmentCode)
-            .expect("Failed to read fragment shader");
-        gShaderFile
-            .read_to_string(&mut geometryCode)
-            .expect("Failed to read geometry shader");
-
-        let vShaderCode = CString::new(vertexCode.as_bytes()).unwrap();
-        let fShaderCode = CString::new(fragmentCode.as_bytes()).unwrap();
-        let gShaderCode = CString::new(geometryCode.as_bytes()).unwrap();
-
-        // 2. compile shaders
-        unsafe {
-            // vertex shader
-            let vertex = gl::CreateShader(gl::VERTEX_SHADER);
-            gl::ShaderSource(vertex, 1, &vShaderCode.as_ptr(), ptr::null());
-            gl::CompileShader(vertex);
-            shader.checkCompileErrors(vertex, "VERTEX");
-            // fragment Shader
-            let fragment = gl::CreateShader(gl::FRAGMENT_SHADER);
-            gl::ShaderSource(fragment, 1, &fShaderCode.as_ptr(), ptr::null());
-            gl::CompileShader(fragment);
-            shader.checkCompileErrors(fragment, "FRAGMENT");
-            // geometry shader
-            let geometry = gl::CreateShader(gl::GEOMETRY_SHADER);
-            gl::ShaderSource(geometry, 1, &gShaderCode.as_ptr(), ptr::null());
-            gl::CompileShader(geometry);
-            shader.checkCompileErrors(geometry, "GEOMETRY");
-
-            // shader Program
-            let ID = gl::CreateProgram();
-            gl::AttachShader(ID, vertex);
-            gl::AttachShader(ID, fragment);
-            gl::AttachShader(ID, geometry);
-            gl::LinkProgram(ID);
-            shader.checkCompileErrors(ID, "PROGRAM");
-            // delete the shaders as they're linked into our program now and no longer necessary
-            gl::DeleteShader(vertex);
-            gl::DeleteShader(fragment);
-            gl::DeleteShader(geometry);
-            shader.ID = ID;
-        }
-
-        shader
     }
 }
